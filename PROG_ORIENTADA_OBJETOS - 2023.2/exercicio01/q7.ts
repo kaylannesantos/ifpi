7d1c66a503e49955657b6dcd76503463cd900cee
Kaylanne Santos <102864770+kaylannemendes@users.noreply.github.com>
2023-08-13 02:30:37 -0300

    update

---
+++ PROG_ORIENTADA_OBJETOS - 2023.2/exercicio01/q7.ts
@@ -0,0 +1,29 @@
+/*7. Considerando o exemplo da classe Retangulo dos slides, implemente um método
+adicional chamado que calcule o perímetro do retângulo e altere a classe
+TestaRetangulo para exibir o cálculo do perímetro. */
+
+class Retangulo {
+    l1: number;
+    l2: number;
+  
+    constructor(l1: number, l2: number) {
+      this.l1 = l1;
+      this.l2 = l2;
+    }
+  
+    calcularArea() {
+      return this.l1 * this.l2;
+    }
+  
+    calcularPerimetro() {
+      return 2 * (this.l1 + this.l2);
+    }
+    
+  }
+  
+  const retangulo = new Retangulo();
+  retangulo.l1 = 12;
+  retangulo.l2 = 3;
+  
+  console.log(retangulo.calcularArea());
+  console.log(retangulo.calcularPerimetro());
