c025c19ff179341f59aac933968cad8b77209b33
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-08-28 08:09:53 -0300

    Update q9.ts

--- PROG_ORIENTADA_OBJETOS - 2023.2/exercicio3/q9.ts
+++ PROG_ORIENTADA_OBJETOS - 2023.2/exercicio3/q9.ts
@@ -1,10 +1,13 @@
 /*9. Crie um exemplo usando a função map para dobrar os elementos de um array e
 reduce para totalizar a soma dos elementos do array. */
 
-function map (numeros: number[]) {
+function mapReduce (numeros: number[]) {
+    //map
     let dobraNumero = numeros.map(num => num * 2);
     console.log(dobraNumero);//log opcional
+    //reduce
     let somaNumero = dobraNumero.reduce((acumulador, numero) => acumulador + numero);
     return somaNumero;
 }
-console.log(map([1,2,3,4,5]))
+
+console.log(mapReduce([1,2,3,4,5]))
