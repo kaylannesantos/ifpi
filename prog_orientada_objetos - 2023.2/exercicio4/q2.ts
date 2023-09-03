a5959e17d266caf1fcb842923ea6f3b17357f237
Kaylanne Santos <102864770+kaylannemendes@users.noreply.github.com>
2023-09-03 16:59:59 -0300

    create

---
+++ prog_orientada_objetos - 2023.2/exercicio4/q2.ts
@@ -0,0 +1,21 @@
+/*2. Suponha uma classe Hotel que sirva apenas para guardar a quantidade de
+solicitações de reservas feitas conforme abaixo:
+class Hotel {
+ quantReservas : number;
+ adicionarReserva() : void {
+ quantReservas++;
+ }
+}
+Podemos afirmar que haverá um problema de compilação, pois a variável inteira não
+foi inicializada previamente? Justifique.
+
+R -> 
+*/
+
+class Hotel {
+    quantReservas : number;
+    adicionarReserva() : void {
+    quantReservas++;
+    }
+   }
+
