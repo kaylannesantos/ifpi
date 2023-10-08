bae7c34bc31a30067748da3ce8cb3d02a87265f0
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-10-07 21:17:29 -0300

    Update

--- prog_orientada_objetos - 2023.2/exercicio5/q2.ts
+++ prog_orientada_objetos - 2023.2/exercicio5/q2.ts
@@ -1,105 +1,25 @@
-/*2. Crie uma implementação que simule um microblog:
-a. Crie uma classe Postagem e nela:
-a. Crie os atributos:
-1. id do tipo number, representando o identificador da
-postagem;
-2. texto do tipo string, representando um texto da postagem;
-3. quantidadeCurtidas do tipo number;
-b. Crie um método chamado curtir que incrementa a quantidade
-curtidas;
-c. Crie um método chamado toString que retorna a concatenação da
-postagem com a quantidade de curtidas;
-b. Crie uma classe Microblog e nela:
-a. Crie um array de classes Postagem;
-b. Crie um método que inclua uma postagem passada como
-parâmetro no array de postagens;
-c. Crie um método de excluir uma postagem que recebe um id
-passado por parâmetro. Para isso, efetue uma busca pelo id nas
-postagens do array e faça a exclusão de forma análoga à feita na
-classe Banco;
-d. Crie um método que retorna a postagem mais curtida;
-e. Crie um método curtir em que se passa um id como parâmetro e a
-classe microblog pesquisa a postagem e chama seu método curtir
-da própria postagem;*/
+/*2. Crie uma classe Hora que tenha:
+a. Três atributos privados e definidos no construtor chamados hora, minutos e
+segundos;
+b. Métodos públicos para ler hora, minuto e segundo de forma individual;
+c. Um método público para retorne a hora no formato “hh:mm:ss”. */
 
-class Postagem {
-    id: number;
-    texto: string;
-    quantCurtidas: number;
+class Hora {
+    private _horas: number;
+    private _minutos: number;
+    private _segundos: number;
 
-    constructor(id: number, texto: string, quantCurtidas: number) {
-        this.id = id;
-        this.texto = texto;
-        this.quantCurtidas = quantCurtidas;
+    constructor(_horas: number, _minutos: number, _segundos: number) {
+        this._horas = _horas;
+        this._minutos = _minutos;
+        this._segundos = _segundos;
     }
 
-    curtir (): void {
-        this.quantCurtidas ++;
-    }
-
-    toString (): string {
-        return "Id: " + this.id + ",Post: " + this.texto + "e Quantidade de likes: " + this.quantCurtidas;
+    horario():string {
+        return this._horas + ":" + this._minutos + ":" + this._segundos;
     }
 }
 
+let h1: Hora = new Hora(10,54,25);
 
-
-class Microblog {
-    postagens: Postagem[] = [];
-
-    consultarPostagem(id: number): number {
-        let indice: number = -1;
-		for (let i: number = 0; i < this.postagens.length; i++) {
-			if (this.postagens[i].id == id) {
-				indice = i;
-				break;
-			}
-		}
-		return indice;
-    }
-
-    inserir (postagem: Postagem): void {//add conta
-        this.postagens.push(postagem);
-    }
-
-    excluir (id: number): void {//remover conta
-        let indice: number = this.consultarPostagem(id);
-
-        if (indice != -1) {
-            for (let i: number = indice; i < this.postagens.length; i++) {
-                this.postagens[i] = this.postagens[i + 1];
-            }
-            this.postagens.pop();
-            console.log(`Postagem com ID ${id} foi excluída.`);
-        }
-    }
-
-    postagemMaisCurtida (): Postagem {
-        let maisCurtidas: number = -1;
-        let postagemMaisCurtida! : Postagem;
-
-        for (let postagem of this.postagens) {
-            if (postagem.quantCurtidas >= maisCurtidas) {
-                postagemMaisCurtida = postagem;
-                maisCurtidas = postagem.quantCurtidas;
-            }
-        }
-        return postagemMaisCurtida;
-    }
-
-    curtir (id: number):void {
-        let postagemProcurada = this.consultarPostagem(id);
-
-        if (postagemProcurada) {
-            this.postagens[postagemProcurada].curtir();
-        }
-    }
-}
-let microblog = new Microblog();
-microblog.inserir(new Postagem(1, "Essa é a 1° postagem", 0));
-microblog.inserir(new Postagem(2, "Essa é a 2° postagem", 3));
-
-microblog.excluir(1);
-microblog.consultarPostagem(2);
-
-console.log(microblog.postagens);
+console.log(`São exatamente: ${h1.horario()}`);
