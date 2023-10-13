4ef28ae150f9c8c399649244968c56ee29ac6ecb
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-10-13 12:17:55 -0300

    Update q1.ts

--- prog_orientada_objetos - 2023.2/exercicio6/q1.ts
+++ prog_orientada_objetos - 2023.2/exercicio6/q1.ts
@@ -1,3 +1,6 @@
+ import PromptSync from "prompt-sync";
+ const input = PromptSync();
+ 
  class Funcionario {
     constructor(private _nome: string, private _idade: number, private _cpf: string, private _valorSalario: number) {
       this._nome = _nome;
@@ -25,6 +28,10 @@
     calcularSalario(): number {
         return this.valorSalario
     }
+
+    toString(): string {
+        return '';
+    }
   }
 
   class Gerente extends Funcionario {
@@ -35,13 +42,19 @@
         this.cargo = cargo;
     }
 
-    calcularSalario(): number {
-        return this.valorSalario + (this.valorSalario * (15/100));
+    calcularSalario(): number { //polimorfismo - sobrescrita
+        let abonoSalarial: number = parseFloat(input("Digite o valor do abono salarial: "));
+        return abonoSalarial + (this.valorSalario + (this.valorSalario * (15/100)));
+    }
+
+    toString(): string {
+        return 'Nome: ' + this.nome + ', ' + 'Idade: ' + this.idade + ', ' + 'Cpf: ' + this.cpf + ', ' + 'Cargo: ' + this.cargo + ', ' + 'Salario: ' + this.valorSalario;
     }
   }
 
   class Supervisor extends Funcionario {
     cargo: string;
+
     constructor(_nome: string, _idade: number, _cpf: string, _valorSalario: number, cargo: string) {
         super(_nome, _idade, _cpf, _valorSalario);
         this.cargo = cargo;
@@ -50,16 +63,25 @@
     calcularSalario(): number {
         return this.valorSalario + (this.valorSalario * (6/100));
     }
+
+    toString(): string {
+        return 'Nome: ' + this.nome + ', ' + 'Idade: ' + this.idade + ', ' + 'Cpf: ' + this.cpf + ', ' + 'Cargo: ' + this.cargo + ', ' + 'Salario: ' + this.valorSalario;
+    }
   }
 
   console.log('\n--------Funcionários----------\n');
 
-  let gerente1: Gerente = new Gerente('Rodrigo', 20, '0000-1', 1500, 'Gerente');
-  console.log(`Nome: ${gerente1.nome}, Idade: ${gerente1.idade},\nCpf: ${gerente1.cpf}, Cargo: ${gerente1.cargo},\nSalario: R$ ${gerente1.calcularSalario().toFixed(2)}`);
+  let g1: Gerente = new Gerente('Rodrigo', 20, '0000-1', 1500, 'Gerente');
+  let g2: Gerente = new Gerente('Mari', 25, '0000-2', 1350, 'Gerente'); 
 
-  console.log('\n');
+  console.log(g1.toString());
+  console.log(g2.toString());
   
-  let supervisor1: Supervisor = new Supervisor('Karol', 19, '0001-1', 1330, 'Supervisor');
-  console.log(`Nome: ${supervisor1.nome}, Idade: ${supervisor1.idade},\nCpf: ${supervisor1.cpf}, Cargo: ${supervisor1.cargo},\nSalario: R$ ${supervisor1.calcularSalario().toFixed(2)}`);
 
-  console.log('\n');
+  console.log('------------------------------------');
+  
+  let s1: Supervisor = new Supervisor('Karol', 19, '0001-1', 1330, 'Supervisor');
+  let s2: Supervisor = new Supervisor('Junior', 21, '0002-2', 1300, 'Supervisor');
+
+console.log(s1.toString());
+console.log(s2.toString());
