803e8758902f30d7ba5bc385b8798417fc2637b3
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-10-16 10:48:04 -0300

    Update q2.ts

--- prog_orientada_objetos - 2023.2/exercicio5/q2.ts
+++ prog_orientada_objetos - 2023.2/exercicio5/q2.ts
@@ -36,23 +36,25 @@ class Postagem {
         this.quantidadeCurtidas = quantidadeCurtidas;
     }
 
-    curtir() {
-        this.quantidadeCurtidas++;
+    curtir(): void {
+        this.quantidadeCurtidas ++;
+    }
+
+    toString(): string {
+        return 'Id: ' + this.id + ', Texto: ' + this.texto + ', Quantidade Curtidas: ' + this.quantidadeCurtidas;
     }
 }
 
 class Microblog {
     postagens: Postagem[] = [];
 
-    incluir(postagem: Postagem) {
+    inserirPostagem(postagem: Postagem): void {
         this.postagens.push(postagem);
     }
 
-    excluir(id: number): void {
-        let indice: number = this.consultarPorIndice(id);
-
-        if (indice != -1) {
-            for (let i: number = indice; i < this.postagens.length; i++) {
+    excluirPostagem(id: number): void {
+        if (id != -1) {
+            for (let i: number = id; i < this.postagens.length; i++) {
                 this.postagens[i] = this.postagens[i + 1];
             }
 
@@ -60,47 +62,10 @@ class Microblog {
         }
     }
 
-    consultarPorIndice(id: number): number {
-        let indice: number = -1;
-		for (let i: number = 0; i < this.postagens.length; i++) {
-			if (this.postagens[i].id == id) {
-				indice = i;
-				break;
-			}
-		}
-		return indice;
-    }
+    postagemMaisCurtida(): Postagem {
 
-    getPostagemMaisCurtida(): Postagem {
-        let maisCurtidas = -1;
-        let postagemMaisCurtida! : Postagem;
-
-        for (let postagem of this.postagens) {
-            if (postagem.quantidadeCurtidas >= maisCurtidas) {
-                postagemMaisCurtida = postagem;
-                maisCurtidas = postagem.quantidadeCurtidas;
-            }
-        }
-        return postagemMaisCurtida;
     }
-
-    curtir(id: number): void {
-        let indicePostagemProcurada = this.consultarPorIndice(id);
-        
-        if (indicePostagemProcurada) {
-            this.postagens[indicePostagemProcurada].curtir();
-        }
-    }
-   
 }
 
-let microblog = new Microblog();
-microblog.incluir(new Postagem(1, "textão 1", 0));
-microblog.incluir(new Postagem(2, "textão 2", 3));
-microblog.incluir(new Postagem(3, "textão 3", 4));
-
-microblog.curtir(3);
-microblog.curtir(3);
-microblog.excluir(1);
-
-console.log(microblog.postagens);
+let p1: Postagem = new Postagem(1, 'Primeira postagem', 3);
+console.log(p1.toString());
