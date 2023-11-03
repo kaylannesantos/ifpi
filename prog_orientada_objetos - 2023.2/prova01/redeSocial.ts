ec91eac7b668e00fd183516e4a3efb77b78dbe0c
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-11-02 23:59:09 -0300

    Update redeSocial.ts

--- prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
+++ prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
@@ -1,20 +1,20 @@
-import { Perfil, RepositorioDePerfis, RepositorioPostagens } from "./index";
+import { Perfil, Postagem, RepositorioDePerfis, RepositorioPostagens } from "./index";
 
 class RedeSocial {
     private _repositorioDePerfis: RepositorioDePerfis;
-    //private _repositorioDePostagens: RepositorioPostagens;, rpostagens: RepositorioPostagens
-    constructor(repositorioDePerfis: RepositorioDePerfis) {
+     private _repositorioDePostagens: RepositorioPostagens;
+    constructor(repositorioDePerfis: RepositorioDePerfis, rpostagens: RepositorioPostagens) {
         this._repositorioDePerfis = repositorioDePerfis;
-        //this._repositorioDePostagens = rpostagens;
+        this._repositorioDePostagens = rpostagens;
     }
 
     get repositorioDePerfis(): RepositorioDePerfis {
         return this._repositorioDePerfis;
     }
 
-    //get respositorioDePostagens(): RepositorioPostagens {
-    //    return this._repositorioDePostagens;
-    //}
+    get respositorioDePostagens(): RepositorioPostagens {
+        return this._repositorioDePostagens;
+    }
 
     incluir(perfil: Perfil): string {
         let perfilExsitente!: Perfil | null;
@@ -34,7 +34,14 @@ class RedeSocial {
         return this.repositorioDePerfis.consultarPerfil(id, nome, email);
     }
 
-    //curtir(idPostagem: number): void {}
+    curtir(idPost: number): string {
+        let postagemProcurada!: Postagem;
+        if (postagemProcurada.idPostagem == idPost) {
+            postagemProcurada.curtir();     
+            return 'Postagem encontrada, curtida com sucesso!';
+        }
+        return 'Postagem não encontrada!';
+    }
 }
 
 let rs: RedeSocial = new RedeSocial(new RepositorioDePerfis())
@@ -46,6 +53,8 @@ console.log(rs.incluir(perfil1));
 console.log(rs.incluir(perfil2)); // o id já existe
 
 console.log(rs.consultar(3));
+
+
 
 
 
