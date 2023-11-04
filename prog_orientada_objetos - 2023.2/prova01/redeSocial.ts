feacf91b746a8e0b6bf18e23fbd67f9ddcf9bff4
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-11-04 11:21:25 -0300

    Update redeSocial.ts

--- prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
+++ prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
@@ -16,86 +16,22 @@ class RedeSocial {
         return this._repositorioDePostagens;
     }
 
-    // PERFIL
-    incluir(perfil: Perfil): string {
-        let perfilExsitente!: Perfil | null;
-
-        if(perfil.idPerfil && perfil.nome && perfil.email){ //incluir perfil a partir do RepositorioDePerfis
-            perfilExsitente = this.repositorioDePerfis.consultarPerfil(perfil.idPerfil, perfil.nome, perfil.email);
-            if(perfilExsitente == null){
-                this.repositorioDePerfis.incluirPerfil(perfil);
-                return 'Perfil incluído com sucesso!';
-            }
-            return 'Perfil já existente!';
-        }
-        return 'Todos os atributos devem estar preenchidos!';
+    incluirPerfil(perfil: Perfil): string | Perfil | null{  // com problema
+        return this.repositorioDePerfis.consultarPerfil(perfil.idPerfil, perfil.nome, perfil.email);
     }
 
-    consultar(id?: number, nome?: string, email?: string): Perfil | string | null{//consultar perfil a partir do RepositorioDePerfis
-        if (!(this.repositorioDePerfis.consultarPerfil(id, nome, email))){
-            return 'Perfil não encontrado!';
-        } 
+    consultarPerfil(id?: number, nome?: string, email?: string): Perfil | string | null {  //consultar perfil a partir do RepositorioDePerfis
         return this.repositorioDePerfis.consultarPerfil(id, nome, email);
     }
 
-    //POSTAGEM 
-    consultarPostagem(id: number, texto: string, hashtag: string | undefined, perfil:  Perfil){
-        if(!this.repositorioDePostagens.consultarPostagem(id, texto, hashtag, perfil)){
-            return 'Postagem não encontrada!'
-        }
+    incluirPostagem(postagem: Postagem): string{
+        return this._repositorioDePostagens.incluirPostagem(postagem);
+    }
 
-        return this.repositorioDePostagens.consultarPostagem(id, texto, hashtag, perfil);
+    consultarPostagem(id?: number | undefined, texto?: string | undefined, hashtag?: string | undefined, perfil?:  Perfil | undefined): Postagem[] | string {
+        return this._repositorioDePostagens.consultarPostagem(id, texto, hashtag, perfil);
     }
 /*
-    // modo 1
-    incluirPostagem(postagem: Postagem): string {
-        let postagemProcurada!: null;
-        
-        if (this.validaPostagem(postagem)) {
-            let postagemExiste = this._repositorioDePostagens.consultarPostagem(postagem.idPostagem);
-
-            if (postagemExiste !== null) {
-                return 'Já existe uma postagem com o mesmo ID!';
-            } else {
-                this._repositorioDePostagens.incluirPostagem(postagem);
-                return 'Post incluido!';
-            }
-        } else {
-            return 'Todos os atributos da postagem devem estar preenchidos!';
-        }
-    
-    }
-
-    // modo 2
-    incluirPostagem2(postagem: Postagem): string {
-        let postagemProcurada!: Postagem | null;
-    
-        if (postagem.idPostagem && postagem.texto && postagem.perfil) {
-            postagemProcurada = this._repositorioDePostagens.consultarPostagem(postagem.idPostagem, postagem.texto, undefined, postagem.perfil);
-            if (!postagemProcurada) {
-                this._repositorioDePostagens.incluirPostagem(postagem);
-                return 'Postagem incluída com sucesso!';
-            }
-        }
-        return 'Todos os atributos devem estar preenchidos!';
-    }
-*/
-    //modo 3
-    incluirPostagem(postagem: Postagem): string {
-        if (this.validaPostagem(postagem)) {
-            let postagemProcurada = this.repositorioDePostagens.consultarPostagem(postagem.idPostagem);
-    
-            if (postagemProcurada === null) {
-                this.repositorioDePostagens.incluirPostagem(postagem);
-                return 'Postagem incluída com sucesso!';
-            } else {
-                return 'Já existe uma postagem com o mesmo ID!';
-            }
-        } else {
-            return 'Todos os atributos da postagem devem estar preenchidos!';
-        }
-    }
-    
     private validaPostagem(postagem: Postagem): boolean {
         return (
             postagem.idPostagem != undefined && 
@@ -103,7 +39,7 @@ class RedeSocial {
             postagem.perfil != undefined
         );
     }       
-
+*/
     // FUNÇÕES DA PÁGINA
     curtir(idPost: number): string {
         let postagemProcurada!: Postagem;
