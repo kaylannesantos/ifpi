aff079322fd9bcdc33a6111725c94118b0d586f4
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-11-07 10:48:12 -0300

    Update redeSocial.ts

--- prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
+++ prog_orientada_objetos - 2023.2/prova01/redeSocial.ts
@@ -41,24 +41,7 @@ class RedeSocial {
     }
 
     exibirPostagensPorHashtag(hashtag: string): PostagemAvancada[] {
-        let postagensFiltradas: PostagemAvancada [] = [];
-        
-        let result = this._repositorioDePostagens.consultarPostagem(undefined, undefined, hashtag, undefined);//como instanciar ????????
-
-        if (typeof result === 'string') {
-            console.log(result);
-            return postagensFiltradas;
-        }
-
-        for(let postagem of result){
-            if (postagem instanceof PostagemAvancada && postagem.existeHashtag(hashtag)){
-                if (postagem.visualizacoesRestantes > 0){
-                    postagensFiltradas.push(postagem);
-                    postagem.decrementarVisualizacoes();
-                }
-            }
-        }
-        return postagensFiltradas;
+        return this.respositorioDePostagens.exibirPostagensPorHashtag(hashtag);
     }
 
     exibirPostagensPorPerfil(id: number): Postagem[]{
@@ -73,46 +56,24 @@ class RedeSocial {
         return this.repositorioDePerfis.exibirTodosOsPerfis();
     }
 
-    exibirPostagensPopulares(): PostagemAvancada[] { // fazer testes
-        let postagensPopulares: PostagemAvancada[] = [];
-        let postagens = this._repositorioDePostagens.consultarPostagem() as PostagemAvancada[]; //downcast => Postagem = PostagemAvancada
+    exibirPorPostagem(idPostagem?: number, texto?: string){
+        return this.respositorioDePostagens.exibirPorPostagem(idPostagem, texto);
+    }
 
-        for (const postagem of postagens) {
-            if (postagem.ehPopular() && postagem.visualizacoesRestantes > 0) {
-                postagensPopulares.push(postagem);
-                postagem.decrementarVisualizacoes();
-            }
-        }
-        return postagensPopulares;
+    postagensPopulares(){
+        return this.respositorioDePostagens.postagensPopulares()
+    }
 
+    excluirPostagem(idPostagem: number){
+        return this.respositorioDePostagens.excluirPostagem(idPostagem)
+    }
+
+    editarNome(antigoNome: string, nomeNovo: string){
+        return this.repositorioDePerfis.editarNome(antigoNome, nomeNovo)
+    }
+
+    editarEmail(antigoEmail: string, emailNovo: string){
+        return this.repositorioDePerfis.editarEmail(antigoEmail, emailNovo)
     }
 }
-let rs: RedeSocial = new RedeSocial();
-let perfil1: Perfil = new Perfil(1, 'alessandra', 'ale@gmail.com')
-let perfil2: Perfil = new Perfil(2, 'kaylanne', 'kay@gmail.com')
-
-let postagem1: Postagem = new Postagem(1, 'texto',perfil1);
-let postagem2: Postagem = new Postagem(2, 'textoo',perfil2);
-
-let rperfil: RepositorioDePerfis = new RepositorioDePerfis();
-let rpostagem: RepositorioDePostagens = new RepositorioDePostagens();
-
-rperfil.incluirPerfil(perfil1);
-rperfil.incluirPerfil(perfil2);
-
-rpostagem.incluirPostagem(postagem1);
-rpostagem.incluirPostagem(postagem2);
-
-rpostagem.curtir(1);
-rpostagem.curtir(1);
-rpostagem.curtir(1);
-rpostagem.descurtir(1);
-rpostagem.descurtir(1);
-rpostagem.descurtir(2);
-rpostagem.curtir(2);
-rpostagem.curtir(2);
-
-console.log(rs.exibirPostagensPopulares());
-
-
 export{ RedeSocial };
