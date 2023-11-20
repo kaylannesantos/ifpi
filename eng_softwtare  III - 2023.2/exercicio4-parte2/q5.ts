1ae156a0283ab7269a4220529f99109628ec0c44
Kaylanne Santos <102864770+KaylanneSantos@users.noreply.github.com>
2023-11-20 13:07:01 -0300

    update

--- eng_softwtare  III - 2023.2/exercicio4-parte2/q5.ts
+++ eng_softwtare  III - 2023.2/exercicio4-parte2/q5.ts
@@ -1,10 +1,9 @@
-interface Publicavel{
+interface Publicavel{ // implementar em todas as classes
     exibir():void;
     getAutor():Perfil;
 }
 
-//Rever a lógica de todas as classes e refatorar
-class Perfil implements Publicavel {
+class Perfil implements Publicavel{
     private _id: number;
     private _nameUser: string;
 
@@ -13,81 +12,95 @@ class Perfil implements Publicavel {
         this._nameUser = nameUser;
     }
 
-    get id():number{
-        return this._id;
-    }
-    get nameUser():string{
-        return this._nameUser;
-    }
+    get id():number{ return this._id;}
+    get nameUser():string{ return this._nameUser;}
 
     exibir(): void {
+        console.log(`Perfil(${this.id}): ${this.nameUser} `);
     }
 
-    getAutor(): Perfil { // rever a lógica deste método
-        return new Perfil(this.id, this.nameUser);
+    getAutor(): Perfil {
+        return this;
     }
+
 }
 
-class Postagem implements Publicavel{
-    private _id: string;
+class Postagem implements Publicavel{ //obs:Postagem tenha uma coleção de reações e comentários
+    private _id: number; //mudei de string para number
     private _autor: Perfil;
     private _conteudo: string;
 
-    constructor(id: string, autor: Perfil, conteudo: string) {
+    constructor(id: number, autor: Perfil, conteudo: string) {
         this._id = id;
         this._autor = autor;
         this._conteudo = conteudo;
     }
 
-    get id(): string {
-        return this._id;
-    }
-    get autor():Perfil{
-        return this._autor;
-    }
-    get conteudo():string{
-        return this._conteudo;
+    get id(): number { return this._id; }
+    get autor():Perfil{ return this._autor; }
+    get conteudo():string{ return this._conteudo; }
+
+    exibir(): void {
+        console.log('POST!');
+        console.log(`Usuário: ${this.autor.nameUser}\nPostagem: ${this.conteudo}`);
     }
 
     getAutor(): Perfil {
         return this.autor;
     }
-
-    exibir(): void {
-        console.log(`Postagem: ${this.id} de ${this.autor.getAutor()}`);
-    }
-    
 }
 
-class Reacao implements Publicavel{
+class Reacao implements Publicavel {
     private _tipoReacao: string;
     private _postagem: Postagem;
-    private _perfil: Perfil;
 
-    constructor(tipoReacao: string, post: Postagem, perfil: Perfil ) {
-        this._postagem = post;
-        this._perfil = perfil;
+    constructor(tipoReacao: string, postagem: Postagem){
         this._tipoReacao = tipoReacao;
+        this._postagem = postagem;
     }
 
-    get tipoReacao():string{
-        return this._tipoReacao;
+    get tipoReacao():string{ return this._tipoReacao};
+    get postagem():Postagem{ return this._postagem};
+
+    exibir(): void {
+        console.log(`Usuário: ${this.postagem.autor}\nPostagem: ${this.postagem.conteudo}\nReação: ${this.tipoReacao}`);
     }
-    get postagem():Postagem{
-        return this._postagem;
+    getAutor(): Perfil {
+        return this.postagem.autor;
     }
-    get perfil():Perfil{
-        return this._perfil;
+}
+
+class Comentario implements Publicavel{
+    private _postagemOriginal: Postagem;
+
+    constructor(postOriginal: Postagem) {
+        this._postagemOriginal = postOriginal;
     }
 
-    getAutor(): Perfil {//qual a lógica desse métodoooooooo??????
+    get postagemOriginal():Postagem{ return this._postagemOriginal}
+
+    exibir(): void {
+        console.log(`Autor: ${this.postagemOriginal.autor}\n Post(${this.postagemOriginal.id}): ${this.postagemOriginal.conteudo}`);
         
     }
 
-    exibir(): void {
-        console.log(`Reação: ${this.tipoReacao} de ${this.perfil.nameUser} na postagem ${this.postagem.conteudo}`);
+    getAutor(): Perfil {
+        return this.postagemOriginal.autor;
     }
-
-
 }
+
+let p1: Perfil = new Perfil(1, 'Maria')
+let p2: Perfil = new Perfil(2, 'João')
+let p3: Perfil = new Perfil(3, 'José')
+
+let post1: Postagem = new Postagem(1, p1, 'eai?')
+let post2: Postagem = new Postagem(2, p2, 'oi?')
+let post3: Postagem = new Postagem(3, p3, 'opa!')
+
+let r1: Reacao = new Reacao('Feliz :)', post1)
+let r2: Reacao = new Reacao('Triste :(', post2)
+let r3: Reacao = new Reacao('Sei lá :|', post3)
+
+console.log(r1.exibir());
+console.log(r1.getAutor());
 
