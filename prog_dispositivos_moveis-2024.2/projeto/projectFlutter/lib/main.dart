f32bc1c8fe84f6117778966fa77a431c4312b321
Kaylanne Santos <102864770+kaylannesantos@users.noreply.github.com>
2025-01-28 11:19:34 -0300

    Geolocalização

--- prog_dispositivos_moveis-2024.2/projeto/projectFlutter/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:new_project/controller/myApp.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-
-
--- prog_dispositivos_moveis-2024.2/projeto/projectFlutter/lib/model/models/ItemModel.dart
+++
@@ -1,7 +0,0 @@
-class ItemModel {
-  String? nome;
-  bool? check;
-
-  //Instanciando a classe
-  ItemModel({this.nome, this.check});
-}
